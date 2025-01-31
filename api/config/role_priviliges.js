95bc1e78d39413beb9fe31399e3a3a80a16a220b
EMRE ŞEN <mail.emresen@gmail.com>
2025-01-31 23:02:55 +0300

    Update role_priviliges.js

--- api/config/role_priviliges.js
+++ api/config/role_priviliges.js
@@ -1,114 +1,107 @@
-module.exports={
-    privGroups:[
+module.exports = {
+    privGroups: [
         {
-            id:"USERS",
-            name:"user permissins"
+            id: "USERS",
+            name: "User Permissions"
         },
         {
-            id:"ROLES",
-            name:"role permissions"
+            id: "ROLES",
+            name: "Role Permissions"
         },
         {
-            id:"CATEGORIES",
-            name:"category permissions"
+            id: "CATEGORIES",
+            name: "Category Permissions"
         },
         {
-            id:"AUDITLOGS",
-            name:"AuditLOGS permissions"
+            id: "AUDITLOGS",
+            name: "AuditLogs Permissions"
         }
     ],
 
-    priviliges: [
+    privileges: [
         {
-            key:"user_view",   //permission: { type: String, required: true },
-            name:"User view",  
-            group:"USERS",
-            description:"User View"
+            key: "user_view",
+            name: "User View",
+            group: "USERS",
+            description: "User view"
         },
         {
-            key:"user_add",
-            name:"User Add",
-            group:"USERS",
-            description:"User Add"
+            key: "user_add",
+            name: "User Add",
+            group: "USERS",
+            description: "User add"
         },
         {
-            key:"user_update",
-            name:"User Update",
-            group:"USERS",
-            description:"User Update"
+            key: "user_update",
+            name: "User Update",
+            group: "USERS",
+            description: "User update"
         },
         {
-            key:"user_delete",
-            name:"User Delete",
-            group:"USERS",
-            description:"User Delete"
+            key: "user_delete",
+            name: "User Delete",
+            group: "USERS",
+            description: "User delete"
         },
         {
-            key:"role_view",
-            name:"Role view",
-            group:"ROLES",
-            description:"Role View"
+            key: "role_view",
+            name: "Role View",
+            group: "ROLES",
+            description: "Role view"
         },
         {
-            key:"role_add",
-            name:"Role Add",
-            group:"ROLES",
-            description:"Role Add"
+            key: "role_add",
+            name: "Role Add",
+            group: "ROLES",
+            description: "Role add"
         },
         {
-            key:"role_update",
-            name:"Role Update",
-            group:"ROLES",
-            description:"Role Update"
+            key: "role_update",
+            name: "Role Update",
+            group: "ROLES",
+            description: "Role update"
         },
         {
-            key:"role_delete",
-            name:"Role Delete",
-            group:"ROLE",
-            description:"Role Delete"
+            key: "role_delete",
+            name: "Role Delete",
+            group: "ROLES",
+            description: "Role delete"
         },
         {
-            key:"category_view",
-            name:"Category view",
-            group:"CATEGORIES",
-            description:"Category View"
+            key: "category_view",
+            name: "Category View",
+            group: "CATEGORIES",
+            description: "Category view"
         },
         {
-            key:"category_add",
-            name:"Category Add",
-            group:"CATEGORIES",
-            description:"Category Add"
+            key: "category_add",
+            name: "Category Add",
+            group: "CATEGORIES",
+            description: "Category add"
         },
         {
-            key:"category_update",
-            name:"Category Update",
-            group:"CATEGORIES",
-            description:"Category Update"
+            key: "category_update",
+            name: "Category Update",
+            group: "CATEGORIES",
+            description: "Category update"
         },
         {
-            key:"category_delete",
-            name:"Category Delete",
-            group:"CATEGORIES",
-            description:"Category Delete"
+            key: "category_delete",
+            name: "Category Delete",
+            group: "CATEGORIES",
+            description: "Category delete"
         },
         {
-            key:"category_export",
-            name:"Category export",
-            group:"CATEGORIES",
-            description:"Category export"
+            key: "category_export",
+            name: "Category Export",
+            group: "CATEGORIES",
+            description: "Category Export"
         },
         {
-            key:"category_import",
-            name:"Category import",
-            group:"CATEGORIES",
-            description:"Category import"
-        },
-        {
-            key:"auditslogs_view",
-            name:"Auditslog view",
-            group:"AUDITLOGS",
-            description:"Auditlogs View"
+            key: "auditlogs_view",
+            name: "AuditLogs View",
+            group: "AUDITLOGS",
+            description: "AuditLogs View"
         }
-   
     ]
 }
